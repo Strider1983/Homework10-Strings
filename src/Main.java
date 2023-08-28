2a0b46fc60b5bc5aa7b3e7f91302237379b81677
Strider <mezentsev.k@gmail.com>
2023-08-28 21:50:54 +0800

    Задача №3 выполнена

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ public class Main {
         System.out.println("Строки");
         System.out.println("________________");
         System.out.println("Задача №1");
-        String name = "Ivan";
-        String middleName = "Ivanovich";
-        String lastleName = "Ivanov";
+        String name = "Семён";
+        String middleName = "Семёнович";
+        String lastleName = "Иванов";
         String fullName = lastleName + " " + name + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println("________________");
         System.out.println("Задача №2");
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("________________");
+        System.out.println("Задача №3");
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
