64d52fa9f3c2b67189b1e8e9143a293c9311744e
Strider <mezentsev.k@gmail.com>
2023-08-28 21:44:42 +0800

    Задача №2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Строки");
+        System.out.println("________________");
+        System.out.println("Задача №1");
         String name = "Ivan";
         String middleName = "Ivanovich";
         String lastleName = "Ivanov";
         String fullName = lastleName + " " + name + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println("________________");
+        System.out.println("Задача №2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
 }
